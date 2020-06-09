9c071077786c002ad0e5b8a04375de41cd8a2161
Thomas <thomas.lima@viacesi.fr>
2020-06-09 19:36:26 +0200

    ll
    ll

---
+++ LESITE/essentials/nav.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+      <a class="navbar-brand" href="#"><i class="fas fa-shield-virus"></i> Covidirect</a>
+
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="navbar-nav container d-flex flex-md-row justify-content-between" id="navbarsExample04">
+
+            <a class="nav-link" href="#">Connexion/Inscrption</a>
+
+      </div>
+    </nav>
