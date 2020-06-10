8ee8589e8cd2e25fca200e2a64bfd5195e3f2309
Thomas <thomas.lima@viacesi.fr>
2020-06-10 14:59:05 +0200

    method post

--- LESITE/DBRELATED/login.php
+++ LESITE/DBRELATED/login.php
@@ -7,21 +7,23 @@
 <div class="container-login">
 
 <div class="text-center">
-    <form class="form-signin">
+    <form class="form-signin" method="POST" action="scriptConnexion.php">
         <i class="fas fa-shield-virus mb-4"></i>
-        <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+        <h1 class="h3 mb-3 font-weight-normal">Veuillez vous connecter</h1>
         <label for="inputEmail" class="sr-only">Email address</label>
-        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-        <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        <input type="email" name="email" class="form-control" placeholder="adresse email" required="required">
+        <label for="inputPassword" class="sr-only"></label>
+        <input type="password" name=pw class="form-control" placeholder="Password" required="required">
         <div class="checkbox mb-3">
         <label>
-        <input type="checkbox" value="remember-me"> Remember me
+        <input type="checkbox" value="remember-me"> Se rappeller de moi
         </label>
         </div>
-        <button class="btn btn-lg btn-secondary btn-block" type="submit">Sign in</button>
-        <p class="mt-5 mb-3 ">Pas encore inscrit ? Cliquez <a href="register.php">ici</a></p>
+        <button class="btn btn-lg btn-secondary btn-block" type="submit">Se connecter</button>
     </form>
+
+    <p class="mt-5 mb-3 ">Pas encore inscrit ? Cliquez <a href="register.php">ici</a></p>
+
 </div>
 
 </div>
--- LESITE/DBRELATED/register.php
+++ LESITE/DBRELATED/register.php
@@ -12,33 +12,33 @@
 <div class="col-md-6">
 <div class="card">
 <article class="card-body">
-<form>
+        <h1 class="h3 mb-3 font-weight-normal"><i class="fas fa-shield-virus mb-4"></i> Veuillez vous enregistrer</h1>
+<form method="POST" action="scriptRegister.php">
 	<div class="form-row">
 		<div class="col form-group">
 			<label>Prénom </label>   
-		  	<input type="text" class="form-control" placeholder="">
+		  	<input type="text" required="required" name="prenom" class="form-control" placeholder="">
 		</div> <!-- form-group end.// -->
 		<div class="col form-group">
 			<label>Nom</label>
-		  	<input type="text" class="form-control" placeholder="">
+		  	<input type="text" required="required" name="nom" class="form-control" placeholder="">
 		</div> <!-- form-group end.// -->
 	</div> <!-- form-row end.// -->
 	<div class="form-group">
 		<label>Adresse Email</label>
-		<input type="email" class="form-control" placeholder="">
+		<input type="email" required="required" name="email" class="form-control" placeholder="">
 		<small class="form-text text-muted">Nous ne partagerons ce mail avec personne d'autre</small>
 	</div> <!-- form-group end.// -->
 	
 	<div class="form-row">
 		<div class="form-group col-md-6">
 		  <label>Département</label>
-		  <input type="text" class="form-control">
+		  <input type="text" required="required" name="userlocation" class="form-control" placeholder="Ex : 78">
         </div> <!-- form-group end.// -->
         
 		<div class="form-group col-md-6">
 		  <label>Photo de profil</label>
-		  <input type="file" class="form-control-file">
-		    
+		  <input type="file" name="userphoto" class="form-control-file">
           </input>
         </div> <!-- form-group end.// -->
         
@@ -47,19 +47,19 @@
 	<div class="form-row">
 		<div class="form-group col-md-6">
 		  <label>Mots de passe</label>
-		  <input type="password" class="form-control">
+		  <input type="password" required="required" name="pw" class="form-control">
         </div> <!-- form-group end.// -->
         
 		<div class="form-group col-md-6">
 		  <label>Confirmer le mots de passe</label>
-		  <input type="password" class="form-control">
+		  <input type="password" required="required" name="pwtest" class="form-control">
         </div> <!-- form-group end.// -->    
     </div> <!-- form-row.// -->
     
     <div class="form-row">
         <button type="submit" class="btn btn-secondary">S'inscrire</button>
     </div>
-
+</form>
 
 </div> 
 <!--container end.//-->
