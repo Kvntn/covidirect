97ea5564e62002ed67d4423ff1aac7a4ff3c5853
Thomas <thomas.lima@viacesi.fr>
2020-06-23 21:20:14 +0200

    css

--- LESITE/chat/chat.php
+++ LESITE/chat/chat.php
@@ -50,6 +50,8 @@
                   <span class="time_date"> 11:01 AM    |    June 9</span></div>
               </div>
             </div>
+
+            
             <div class="outgoing_msg">
               <div class="sent_msg">
                 <p>Test which is a new approach to have all
