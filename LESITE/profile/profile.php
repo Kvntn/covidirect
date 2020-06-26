2c44ce918c7e3315ce7fb7ab5f93048bbdcb847c
Thomas <thomas.lima@viacesi.fr>
2020-06-26 18:40:59 +0200

    modifier profil

--- LESITE/profile/profile.php
+++ LESITE/profile/profile.php
@@ -29,20 +29,67 @@
 
 
 
-    <div class="card-body tab-pane active" id="p1">        
-        <div class="container-profil">
-        <div class="card" style="width: 35rem;">
-            <div class="card-title text-center">
-                <img src="../resources/images/thomas.jpg" alt="..." class="img rounded-circle"> <?php echo $_SESSION['nom'],' ', $_SESSION['prenom']; ?>
-            </div>
+    <div class="card-body tab-pane active" id="p1"> 
 
+    <div class="row">
+        <div class="col-sm-6">
+            <div class="card">
             <div class="card-body">
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item"><i class="fas fa-map-pin"></i> <?php echo $_SESSION['userlocation']; ?> </li>
-                </ul>
+                <h5 class="card-title"><img src="../resources/images/thomas.jpg"><?php echo $_SESSION['nom'],'    ',$_SESSION['prenom'];?></h5>
+                <li class="list-group-item"><i class="fas fa-map-pin fa-lg"></i>    Localisation de l'utilisateur : <?php echo $_SESSION['userlocation']?></li>
+            </div>
             </div>
         </div>
+        <div class="col-sm-6">
+            <div class="card">
+                <div class="card-header">
+                    <h4>Modifier votre profil</h4>
+                </div>
+            <div class="card-body">
+            <form action="profileUpdate.php">
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item"><div class="row">
+                    <div class="col">
+                    <label>Nom</label>
+                    <input type="text" class="form-control" name="nom" required="required" placeholder="<?php echo $_SESSION['nom']?>">
+                    </div>
+                    <div class="col">
+                    <label>Prenom</label>
+                    <input type="text" class="form-control" required="required" name="prenom" placeholder="<?php echo $_SESSION['prenom']?>">
+                    </div>
+                </div></li>
+                <li class="list-group-item"><div class="form-row">
+                    <div class="form-group col-md-6">
+                    <label>Email</label>
+                    <input type="email" required="required" class="form-control" id="inputEmail4" placeholder="<?php echo $_SESSION['email']?>">
+                    </div>
+                    <div class="form-group col-md-6">
+                    <label>Nouveau mot de passe</label>
+                    <input type="password" required="required" class="form-control" id="inputPassword4" placeholder="Nouveau mot de passe">
+                    </div>
+                </div></li>
+                <li class="list-group-item"><div class="form-row">
+                    <div class="form-group col-md-6">
+                    <label>Département</label>
+		            <input type="text" required="required" name="userlocation" class="form-control" placeholder="<?php echo $_SESSION['userlocation']?>">
+                    </div>
+                    <div class="form-group col-md-6">
+                    <label>Photo de profil</label>
+                    <input type="file" name="userphoto" class="form-control-file"></input>
+                    </div>
+                </div></li>
+                <li class="list-group-item">
+                <p class="signin button">
+                <input type="submit" class="btn btn-secondary"value="Modifier le profile"/>
+                </p>
+                </li>
+            </ul>  
+            </div>
+            </form> 
+            </div>
         </div>
+    </div>
+
     </div pan1>
 
     <div class="card-body tab-pane" id="p2">
