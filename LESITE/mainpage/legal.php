ae4b07e65ec4e43bea4e2bfa6c52cbfcd50a15c2
Thomas <thomas.lima@viacesi.fr>
2020-06-28 16:22:15 +0200

    legal fix

--- LESITE/mainpage/legal.php
+++ LESITE/mainpage/legal.php
@@ -1,9 +1,9 @@
 <!doctype html>
 <html lang="en">
 <?php  
-    include("header.php");
-    include("nav.php");
-    include("footer.php");
+    include("../essentials/header.php");
+    include("../essentails/nav.php");
+    include("../essentials/footer.php");
 ?>
 
 <div class="legal-container">
@@ -25,12 +25,10 @@ e-mail :
 
 <h1>Respect de la vie privée et collecte des Données Personnelles<strong><br> </strong></h1>
 <p>Soucieux de protéger la vie privée de ses clients, CESI s’engage dans la protection des données personnelles. Une politique sur la protection des données personnelles rappelle nos principes et nos actions visant au respect de la réglementation applicable en matière de protection des données à caractère personnel.</p>
-<p><a href="privacy_policy.php">Pour lire l’intégralité de notre politique sur la Protection des données personnelles cliquez-ici</a>
 </p><h1>Sécurité</h1>
 <p>Le CESI s’engage à mettre en œuvre tous les moyens nécessaires au bon fonctionnement du site. Cependant, le CESI ne peut pas garantir la continuité absolue de l’accès aux services proposés par le site. Les adhérents sont informés que les informations et services proposés sur le site pourront être interrompus en cas de force majeure et pourront le cas échéant contenir des erreurs techniques.</p>
 <h1>Utilisation de cookies<strong><br> </strong></h1>
 <p>Des cookies sont utilisés sur nos sites.</p>
-<p><a href="privacy_policy.php">Pour plus d’informations, vous pouvez vous référer à la Politique sur le Protection des Données Personnelles en cliquant-ici</a></p>
 <h1>Déclarations d’activité</h1>
 <p>CESI SAS – Société par actions simplifiée au capital de 1.1M€
 <br> 342 707 502
